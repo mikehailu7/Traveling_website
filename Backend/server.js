e4ae55cc2bf902f51055c4b37f079d4ff2458398
mikehailu7 <mikehailu7@gmail.com>
2024-08-29 17:29:16 +0300

    clean up code

--- Backend/server.js
+++ Backend/server.js
@@ -7,7 +7,7 @@ dotenv.config({ path: './.env' });
 const { isDev } = require('./src/utils/environment');
 const { seedDB } = require('./importData');
 
-/** connect to database */
+/** connection to mongo db database */
 mongoose.connect(isDev() ? process.env.LOCAL_DB_URI: process.env.REMOTE_DB_URI,
     {
         useNewUrlParser: true,
@@ -22,8 +22,6 @@ mongoose.connect(isDev() ? process.env.LOCAL_DB_URI: process.env.REMOTE_DB_URI,
         console.log("Database connection error!\n", e)
     });
 
-
-/** seed the database */
 if(!isDev()){
     seedDB(true);
 }
--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,17 +1,15 @@
-const express = require("express");
-const monogoSanitize = require("express-mongo-sanitize");
-const cors = require("cors");
-const userRouter = require("./routes/customerRoute");
+
+const helmet = require("helmet");
+const xssClean = require("xss-clean");
+const hpp = require("hpp");
+const cookieParser = require("cookie-parser");
 
 const imageRouter = require("./routes/image_route");
 const http = require("http");
 const socket = require("socket.io");
 const { isDev } = require('./utils/environment');
 
-const helmet = require("helmet");
-const xssClean = require("xss-clean");
-const hpp = require("hpp");
-const cookieParser = require("cookie-parser");
+
 
 
 const AppError = require("./utils/appError");
@@ -21,6 +19,10 @@ const authRouter = require("./routes/authenticationroute");
 const houseRouter = require("./routes/place_route");
 const reviewRouter = require("./routes/reviewRoute");
 
+const express = require("express");
+const monogoSanitize = require("express-mongo-sanitize");
+const cors = require("cors");
+const userRouter = require("./routes/customerRoute");
 
 
 const app = express();
@@ -32,63 +34,33 @@ const io = socket(server, {
   },
 });
 
-/**
- * using middlewares
- */
 app.use(cors());
 
-//for logging purposes
-// if (isDev()) {
-//   app.use(morgan("dev"));
-// }
-
-// use cookie parser to get nicely formatted cookie
 app.use(cookieParser());
 
-// read and parse json data from request body & limit size of request data
 app.use(express.json({ limit: "10kb" }));
 
-// sanitize request body for NoSql injection attacks
 app.use(monogoSanitize());
 
-// sanitize request body for NoSql injection attacks
 app.use(xssClean());
 
-// prevent parameter pollution attacks
 app.use(hpp());
 
-// header sender helmet for security | security http headers
 app.use(helmet());
 
-/**
- * registering middlewares
- */
-app.use("/api/v1/users", userRouter);
-app.use("/api/v1/auth", authRouter);
-app.use("/api/v1/houses", houseRouter);
+app.use("/api/v1/customer", customerRouter);
+app.use("/api/v1/authentication", authenticationrouter);
+app.use("/api/v1/place", placerouter);
 app.use("/api/v1/reviews", reviewRouter);
 app.use("/api/v1/image", imageRouter);
 
-/**
- * handle unregisterd routes
- */
 app.use("*", (req, res, next) => {
   next(
     new AppError(`The requested url ${req.originalUrl} does not exist`, 404)
   );
 });
 
-/**
- * general error handling middleware
- * express redirects route to this function
- * if error occured i.e argument passed into
- * the next() function.
- */
 app.use(globalErrorHandler);
-
-/**
- * Socket
- */
 
 io.on("connection", (socket) => {
   socket.on("join", (ids) => {
